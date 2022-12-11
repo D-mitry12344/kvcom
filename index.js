648d5dcfa0cc43349e4917eb6f77a1650664cd36
D-mitry12344 <114662273+D-mitry12344@users.noreply.github.com>
2022-12-11 16:02:36 +0300

    coockieMod

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let phone = document.querySelector(".phone");
 let but = document.querySelector(".butt");
-let check =document.querySelector(".check")
+let check =document.querySelector(".check");
+document.cookie = "user=John"; // обновляем только куки с именем 'user'
+console.log(document.cookie); 
+
+/*
 let go = function(){
 let hone = phone.value;
 localStorage.setItem('hone', hone);
+document.cookie = "user=MARKA"
 console.log(localStorage.getItem('hone'));
+alert(document.cookie);
 
 };
 
@@ -12,3 +18,13 @@ but.onclick = function(){
 go();
 window.location.href = 'indexPass.html';
 };
+
+let mass = new Array([1,1,2,3,4]);
+console.log(mass)
+let mas = new String(1);
+console.log(mas)
+let ma = 1;
+console.log((1));
+Object.prototype.sayHello = function() {
+  console.log('Hello!')
+}*/
