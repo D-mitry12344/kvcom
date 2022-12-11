21774a939af8d48a40dba01ee221ec75b88b5092
D-mitry12344 <114662273+D-mitry12344@users.noreply.github.com>
2022-12-11 16:12:40 +0300

    modCookie

--- indexPass.js
+++ indexPass.js
@@ -2,10 +2,13 @@ let nuber = document.querySelector(".nuber");
 nuber.textContent = '+7-'+ localStorage.getItem('hone').slice(1,4) + '-' +localStorage.getItem('hone').slice(4,7) + '-' + '****';
 let pass = document.querySelector(".pass");
 let button = document.querySelector(".buttt");
-let passwords = [];
-button.onclick = function() {
-passwords.push(pass.value);
-localStorage.setItem('pass', passwords);
-console.log(localStorage.getItem('pass'));
-return passwords;
+let user = {};
+let s = 0;
+button.onclick = () =>{
+	user.login = phone.value;
+	user.pass = pass.value;
+	s += 1;
+	let users = JSON.stringify(user);
+	document.cookie= s+ "=" +users
+	return s;
 }
