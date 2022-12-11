b85ff58874b3e4a3e9ae6b89a7195dbcbed6dfce
D-mitry12344 <114662273+D-mitry12344@users.noreply.github.com>
2022-12-11 16:46:59 +0300

    geter

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 let phone = document.querySelector(".phone");
 let but = document.querySelector(".butt");
-document.cookie = "user=John";
-console.log(document.cookie);
 go = () =>{
   let hone = phone.value;
   localStorage.setItem('hone' , hone);
